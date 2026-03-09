d01d8d3043d9c31ecd053a22195147df81ae79e0
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-03-09 22:33:58 +0530

    to get even and odd numbers

--- PracticeProblems/PracticeProblems.java
+++ PracticeProblems/PracticeProblems.java
@@ -4,19 +4,20 @@ public class PracticeProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter employee salary: ");
-        double salary = sc.nextDouble();
+        System.out.print("Enter a natural number: ");
+        int number = sc.nextInt();
 
-        System.out.print("Enter years of service: ");
-        int years = sc.nextInt();
-
-        double bonus = 0;
-
-        if (years > 5) {
-            bonus = salary * 0.05;
-            System.out.println("Bonus amount: " + bonus);
+        // Check if natural number
+        if (number <= 0) {
+            System.out.println("Please enter a natural number (greater than 0).");
         } else {
-            System.out.println("No bonus applicable.");
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is Even");
+                } else {
+                    System.out.println(i + " is Odd");
+                }
+            }
         }
 
         sc.close();
