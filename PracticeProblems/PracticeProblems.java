ce03a8a81bfe3308a06a9702cd7fa95086330d43
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-03-09 22:23:43 +0530

    to get largest number among three numbers

--- PracticeProblems/PracticeProblems.java
+++ PracticeProblems/PracticeProblems.java
@@ -4,14 +4,18 @@ public class PracticeProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
+        System.out.print("Enter first number: ");
+        int number1 = sc.nextInt();
 
-        if (number % 5 == 0) {
-            System.out.println("Is the number " + number + " divisible by 5? Yes");
-        } else {
-            System.out.println("Is the number " + number + " divisible by 5? No");
-        }
+        System.out.print("Enter second number: ");
+        int number2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int number3 = sc.nextInt();
+
+        System.out.println("Is the first number the largest? " + (number1 > number2 && number1 > number3));
+        System.out.println("Is the second number the largest? " + (number2 > number1 && number2 > number3));
+        System.out.println("Is the third number the largest? " + (number3 > number1 && number3 > number2));
 
         sc.close();
     }
