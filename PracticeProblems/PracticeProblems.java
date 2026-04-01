21488d8f137cda61e403a696424c67dffce2d9e8
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-04-01 21:39:13 +0530

    branch3:to check wheather the number is positive,even,odd,negative and to check the largest number

--- PracticeProblems/PracticeProblems.java
+++ PracticeProblems/PracticeProblems.java
@@ -4,23 +4,39 @@ public class PracticeProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] ages = new int[10];
+        int[] numbers = new int[5];
 
-        // Taking input
-        System.out.println("Enter ages of 10 students:");
-        for (int i = 0; i < ages.length; i++) {
-            ages[i] = sc.nextInt();
+        // Input
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
         }
 
-        // Checking eligibility
-        for (int i = 0; i < ages.length; i++) {
-            if (ages[i] < 0) {
-                System.out.println("Invalid age: " + ages[i]);
-            } else if (ages[i] >= 18) {
-                System.out.println("The student with age " + ages[i] + " can vote.");
+        // Checking each number
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 0) {
+                if (numbers[i] % 2 == 0) {
+                    System.out.println(numbers[i] + " is positive and even.");
+                } else {
+                    System.out.println(numbers[i] + " is positive and odd.");
+                }
+            } else if (numbers[i] < 0) {
+                System.out.println(numbers[i] + " is negative.");
             } else {
-                System.out.println("The student with age " + ages[i] + " cannot vote.");
+                System.out.println(numbers[i] + " is zero.");
             }
+        }
+
+        // Compare first and last element
+        int first = numbers[0];
+        int last = numbers[numbers.length - 1];
+
+        if (first == last) {
+            System.out.println("First and last elements are equal.");
+        } else if (first > last) {
+            System.out.println("First element is greater than last.");
+        } else {
+            System.out.println("First element is less than last.");
         }
 
         sc.close();
