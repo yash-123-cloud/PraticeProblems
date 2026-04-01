9b47ae58724417bbcc88b32ca37d1eafdaf8d22b
yeswanthreddyv-pixel <yeswanthreddy8686.v@gmail.com>
2026-04-01 21:55:34 +0530

    branch3:to know the youngest person among friends

--- PracticeProblems/PracticeProblems.java
+++ PracticeProblems/PracticeProblems.java
@@ -4,20 +4,41 @@ public class PracticeProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] age = new int[3];
+        double[] height = new double[3];
 
-        int[] table = new int[10];
+        // Input
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter details for " + names[i]);
 
-        // Store multiplication results
-        for (int i = 1; i <= 10; i++) {
-            table[i - 1] = number * i;
+            System.out.print("Enter Age: ");
+            age[i] = sc.nextInt();
+
+            System.out.print("Enter Height: ");
+            height[i] = sc.nextDouble();
         }
 
-        // Display table
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(number + " * " + i + " = " + table[i - 1]);
+        // Find youngest
+        int youngestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < age[youngestIndex]) {
+                youngestIndex = i;
+            }
         }
+
+        // Find tallest
+        int tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (height[i] > height[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Output
+        System.out.println("\n--- Results ---");
+        System.out.println("Youngest Friend: " + names[youngestIndex] + " (Age: " + age[youngestIndex] + ")");
+        System.out.println("Tallest Friend: " + names[tallestIndex] + " (Height: " + height[tallestIndex] + ")");
 
         sc.close();
     }
